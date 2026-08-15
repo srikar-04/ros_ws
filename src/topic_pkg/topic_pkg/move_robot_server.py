7ad914215638c534244063c53647e030ae102aa1
srikar <srikar.code01@gmail.com>
2026-08-15 21:13:19 +0530

    modified action server from sigle thread to multi-thread and from mutually exclusive callback to reentrant callback group

--- src/topic_pkg/topic_pkg/move_robot_server.py
+++ src/topic_pkg/topic_pkg/move_robot_server.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer
+from rclpy.action import ActionServer, GoalResponse, CancelResponse
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 from my_robot_interfaces.action import MoveRobot
 
@@ -10,6 +12,8 @@ import time
 class MoveRobotServer(Node):
     def __init__(self):
         super().__init__("move_robot_server")
+
+        self.callback_group = ReentrantCallbackGroup()
 
         # Action server is an api provided by ros2
         # It takes 
@@ -21,40 +25,68 @@ class MoveRobotServer(Node):
             self,
             MoveRobot,
             "/move_robot",
-            self.execute_callback,
+            execute_callback = self.execute_callback,
+            goal_callback = self.goal_callback,
+            cancel_callback = self.cancel_callback,
+            callback_group = self.callback_group
         )
 
         self.get_logger().info(
             "Move Robot Action Server Is Ready"
         )
 
-    # This is not a normal callback like topic or service. It is a goal callback, it recieves goal_handle which respresents the particular goal/target sent by the client and it is also capable of sending feedback and cancellation response to client. One the goal is completed, it can also send result to client.
+    # goal_callback and cancel_callbakc are merly used for accepting and rejecting respecitve goals and cacellation requests
+
+    # When client gives a goal/target to server, the control first reaches the goal_callback. goal_callback has the authority to accept or reject the goal. If the goal is accepted by goal_callback then the control reaches to execute callback where the actual execution happens based on the target sent by client. 
+
+    # If there is no goal_callback, ROS2 by default accepts all the incoming goals
+    def goal_callback(self, goal_request) :
+
+        self.get_logger().info(
+            f"Revieved goal request : {goal_request.target}"
+        )
+
+        return GoalResponse.ACCEPT
+
+    # When there is a cancellation request, it first reaches the cancel_callback, this callback has the ability to either accept or reject the cancellation request. If cancellation request is accepted then the if condition with "is_handle_requested" becomes true in the execution callback.
+    def cancel_callback(self, cancel_request):
+        self.get_logger().info(
+            f"Recieved cancellation request"
+        )
+
+        return CancelResponse.ACCEPT
+
+    # IMPORTANT : So these goal and cancel callbacks are just decision callbacks, they do not perform any execution. Execution is done inside exection callback
+
+    # This is not a normal callback like topic or service. It is a goal callback, it recieves goal_handle which respresents the particular goal/target sent by the client and it is also capable of sending feedback response to client. Once the goal is completed, it can also send result to client. 
 
     def execute_callback(self, goal_handle):
         target = goal_handle.request.target
 
         self.get_logger().info(
-            f"Recieved goal : {target}"
+            f"Executing goal : {target}"
         )
 
         feedback = MoveRobot.Feedback()
+        result = MoveRobot.Result()
 
         for i in range(1, 11):
-
-            time.sleep(1)
 
             if goal_handle.is_cancel_requested:
 
                 self.get_logger().info(
-                    "Cancellation requested."
+                    "Cancellation requested. Stopping Goal"
                 )
                 
                 # This tells ros that i have accepted cancellation request and the gaol is now cancelled
 
                 goal_handle.canceled()
 
-                result = MoveRobot.Result()
                 result.success = False
+
+                self.get_logger().info(
+                    f"Goal Canceled"
+                )
 
                 return result
 
@@ -66,10 +98,11 @@ class MoveRobotServer(Node):
                 f"Progress: {feedback.progress}%"
             )
 
+            time.sleep(1)
+
             # rate.sleep()
         goal_handle.succeed()
 
-        result = MoveRobot.Result()
         result.success = True
 
         self.get_logger().info(
@@ -83,9 +116,20 @@ def main(args=None):
 
     node = MoveRobotServer()
 
-    rclpy.spin(node)
-    
-    node.destroy_node()
+    executor = MultiThreadedExecutor(
+        num_threads=2
+    )
+
+    executor.add_node(node)
+
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        executor.shutdown()
+        node.destroy_node()
+        rclpy.shutdown()
 
     rclpy.shutdown()
 if __name__ == "__main__" :
